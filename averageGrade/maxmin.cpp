029ba90c4d4cb6bbb09e18719188318ef23f0cfb
andrewkaldani <59716037+andrewkaldani@users.noreply.github.com>
2020-04-22 12:57:00 -0700

    Create maxmin.cpp

---
+++ averageGrade/maxmin.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+  double average = 0;
+  double number = 0;
+  double grade = 0;
+  double total = 0;
+  double largestNumber=30;
+  double smallestNumber=0;
+  double i=1;
+
+
+
+  cout<<"Enter the amount of quizzes: "<<endl;
+  cin>>number;
+  while (i<=number)
+  {
+  cout<<"Input grades: "<<endl;
+  cin>>grade;
+  if((grade == -1) || (grade > 30))
+  {
+    cout<<"enter a valid number! Re-enter a score"<<endl;
+    cin>>grade;
+}
+largestNumber = grade;
+smallestNumber = grade;
+  if (grade > largestNumber)
+  {
+    largestNumber = grade;
+  }
+  if (grade < smallestNumber)
+  {
+     smallestNumber = grade;
+  }
+  total+=grade;
+  i++;
+  }
+  average = total / number;
+  cout<<"The average is "<<average<<" points on this exam"<<endl;
+  cout<<"Smallest: "<<smallestNumber<<endl;
+  cout<<"largest: "<<largestNumber<<endl;
+  return 0;
+
+}
